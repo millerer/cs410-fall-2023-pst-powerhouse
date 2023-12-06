ae23269e9cfff860b55f6e3f016bade9ab8b3e5f
arjunb2112 <arjun12@illinois.edu>
2023-12-05 22:01:19 -0600

    Integrating UI with rest of code

--- UI.py
+++ UI.py
@@ -1,20 +1,21 @@
 import tkinter as tk
+from main import calculate_list
 
 
 def display_results():
-    print(entry.get())
-    results_label["text"] = "results:"
-    '''TODO make this generate a list using other code'''
-    results["text"] = "Pokemon SoulSliver\n" \
-                      "Legend of Zelda: Breath of the Wild\n" \
-                      "Super Smash Bros: Brawl\n" \
-                      "Crono Trigger\n" \
-                      "Super Mario Galaxy 2\n" \
-                      "Hades\n" \
-                      "Ace Attorney\n" \
-                      "Super Mario Bros 3\n" \
-                      "Celeste\n" \
-                      "Hollow Knight"
+    if entry.get() == '':
+        results_label["text"] = "No Query Detected"
+    else:
+        results_label["text"] = "Calculating Results:"
+        result = calculate_list(entry.get())
+        print(result)
+        result_list = list(result["app_name"])
+        result_string = ''
+        for item in result_list:
+            result_string += item
+            result_string += '\n'
+        results_label["text"] = "Results:"
+        results["text"] = result_string
 
 
 if __name__ == '__main__':
